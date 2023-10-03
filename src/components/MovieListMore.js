b12628afe9a211cd109e0a07212026831d4d1ec9
yjkim <k.yeojin15@gmail.com>
2023-10-03 17:18:31 +0900

    update view more function

--- src/components/MovieListMore.js
+++ src/components/MovieListMore.js
@@ -15,6 +15,14 @@ export default class MovieListMore extends Component {
   }
   render() {
     this.el.classList.add('btn', 'view-more', 'hide');
+    const { page, pageMax, searchText } = movieStore.state;
+
+    if (searchText && pageMax > page) {
+      this.el.classList.remove('hide');
+    } else {
+      this.el.classList.add('hide');
+    }
+
     this.el.textContent = 'View more..';
     this.el.addEventListener('click', async () => {
       this.el.classList.add('hide');
